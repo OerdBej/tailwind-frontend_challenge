e5293a6f78a405b0ed56be67f9c3f8e9df6ea792
Oerd Bej <oerdbej@gmail.com>
2023-07-02 13:26:39 +0200

    hero section for the main menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./*.html'],
     theme: {
         screens: {
-            sm: '480',
-            md: '768',
-            lg: '976',
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
             xl: '1440px',
         },
         extend: {
             fontFamily: {
                 sans: ['Josefin Sans', 'sans-serif'],
                 alata: ['Alata'],
+            },
+            letterSpacing: {
+                widest: '.3em',
             },
         },
     },
